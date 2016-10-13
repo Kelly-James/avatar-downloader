a0ab68e846f0464d1f3978695ba1dec69510217f
Kelly James Clancy <kellyjamesclancy@gmail.com>
2016-10-13 03:48:49 +0000

    Deleted unused code, ensured proper indenting/spacing, added comments, renamed function(s)/variable(s),

--- avatarDl.js
+++ avatarDl.js
@@ -1,9 +1,12 @@
+// AVATAR DOWNLOADER
+
+// state modules,
 var request = require('request');
 var http = require('http');
 var fs = require('fs');
 
-
-// require('dotenv').config();
+// set variables, awaiting input from terminal,
+// state options for url request,
 var user = process.argv[2];
 var repo = process.argv[3];
 var contributors = process.argv[4];
@@ -14,11 +17,11 @@ var options = {
   },
   json: true,
 }
-console.log(options);
-console.log('user: ' + user);
-console.log('repo: ' + repo);
 
-
+// make request to url,
+// loop through returned object,
+// pull login name and avatar url,
+// call writeImage function,
 function getRepoContributors() {
   request(options, function(error, response, body) {
     var user = '';
@@ -27,69 +30,21 @@ function getRepoContributors() {
       for(var value of body) {
         user = value.login + '.png';
         avaUrl = value.avatar_url;
-        downloadImageByURL(avaUrl, user);
+        writeImage(avaUrl, user);
       }
     }
-    console.log(user);
-    console.log(avaUrl);
   });
 }
 
-function downloadImageByURL(url, filepath) {
-  // console.log(url);
-  // console.log('avatars/' + filepath);
+// write info to disk,
+function writeImage(url, filepath) {
   request(url).pipe(fs.createWriteStream(repo + '/' + filepath));
 }
 
+// create directory,
+// call getRepoContributors function,
 fs.mkdir(repo, function() {
   getRepoContributors();
 });
-
-
-
-  // for(var cont of body) {
-  //   console.log(cont);
-  // }
-  // console.log(body);
-
-  // var magic_array = [
-  //   {name: 'ggrochow', url: 'https://avatars.githubusercontent.com/u/11564146?v=3'},
-  // ]
-
-
-
-
-
- //  var data = url;
- //  writerStream.end();
- //  writerStream.on('finish', function() {
- //    console.log('Write Completed');
- //  });
- //  writerStream.on('error', function(err) {
- //    console.log(err.stack);
- //  });
- // console.log('Program Ended');
-
-// function getRepoContributors(repoOwner, repoName, dealWithContributors) {
-//   var token = 0e7db6124dcaab4eedb3ea195007487183c3b6f3;
-//   var contributors = [];
-//   dealWithContributors(contributors);
-//   return contributors;
-// }
-
-// function getURLData(url, request) {
-//   request(url, function(error, res, body) {
-//     if (error) {
-//       return
-//     }
-//     request(body);
-//   });
-// }
-
-// function downloadImageByURL(url, filePath) {
-
-// }
-
-// console.log(contributors);
 
 
